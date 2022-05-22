cc6372a3134efcb7f732852b2812ae58c88da620
Roman <romantsiukh@gmail.com>
2022-05-22 17:53:03 +0300

    Task_2

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsEl = d
+const ingredEl = document.querySelector(`#ingredients`);
+
+const elements = ingredients.map(ingredient => {
+  const ingredientsEl = document.createElement(`li`);
+        ingredientsEl.classList.add(`item`);
+        ingredientsEl.textContent = ingredient;
+
+        return ingredientsEl;
+})
+
+console.log(elements)
+ingredEl.append(...elements)
